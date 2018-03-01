28548d2f82958a6c7eaf539cdf9d840b16fbe700
Nicolas Broguiere <broguiere.nicolas@gmail.com>
2018-03-01 13:53:42 +0100

    Reorganized the comments in the master file for more clarity.

--- master_template.m
+++ master_template.m
@@ -3,8 +3,7 @@ addpath(genpath('copy_TFM_release1_location_here'));
 addpath(genpath('copy_BioFormats_location_here'));
 
 % Information about the video to be analyzed:
-% (Prepare the video including the reference frame in fiji, 
-% exported as a .tif 8 bit composite hyperstack) 
+% (Video including the reference frame, exported as a .tif 8 bit composite hyperstack from Fiji) 
 input_stack='my_raw_data.tif';
 n_colors=2;         % Total number of color channels
 n_timepoints=2;     % Total number of timepoints including the reference frame
@@ -16,7 +15,7 @@ pxsize_xy=0.2;      % in um (micrometers)
 pxsize_z=0.4;       % in um
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Cell segmentation parameters:
+%                  Cell segmentation parameters:                          %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Warning: large values and spherical structuring elements can make the segmentation calculations quickly become untractable. Recommended to start with small values. 
 sigma=1;                    % In um, standard deviation of the 3D Gaussian smoothing applied to the image before segmentation.
@@ -30,92 +29,82 @@ rejection_radius=2;         % In um. If above 0, experimental displacements clos
 segment_reference_frame=0;  % If the reference frame is a killed cell, no need for segmentation (save time). If the reference frame is just like another frame, and median displacement is then subtracted, then needs to be segmented like any other. 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Parameters for the matrix displacement tracking by cross-correlation and for the filtering: 
+%  Parameters for the matrix displacement tracking by cross-correlation:  %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-box_size_xy=41;     %px, odd number.
-box_size_z=41;       %px, odd number.
-step_size_xy=21;
-step_size_z=21;
-max_distance_correlation_xy=20;  % px
-max_distance_correlation_z=20;   % px
-reference_frame=2;%floor(n_timepoints/2);  % Number of the reference frame, or -1 for relative to previous timepoint. 
-% Cross-correlation is between -1 (opposite) through 0 (random, not
-% matching) to 1 (perfect matching). This parameter sets the cross-correlation
-% under which results are rejected for lack of confidence in the matching:
-autocorrelation_threshold=0.7;
-% Subtract median of the layer to each layer (to compensate for drif):
-subtract_median_layer=1;
-% In the time direction. Should compute integrals typically if the reference frame is the previous frame.
-compute_integrals=0;    
-% If computing integrals or if the reference frame is not a relaxed frame,
-% should subtract the time-median position considered as the relaxed one:
-subtract_median_position=0;
+box_size_xy=41;                 % in px, odd number. Size of the small 3D boxes tracked in the matrix. 
+box_size_z=41;                  % in px, odd number.
+step_size_xy=21;                % in px. Spacing of the array on which displacement values are collected. 
+step_size_z=21;                 % in px.
+max_distance_correlation_xy=20; % in px. Size of the search range (max displacement of a box). 
+max_distance_correlation_z=20;  % in px.
+reference_frame=2;              % Position of the reference frame, or -1 for tracking relative to the previous timepoint for each frame (not recommended). 
+autocorrelation_threshold=0.7;  % Cross-correlation is between -1 (opposite) through 0 (random, not matching) to 1 (perfect matching). This parameter sets the cross-correlation under which results are rejected for lack of confidence in the matching.
+subtract_median_layer=1;        % Subtract median displacement of each layer to this layer (to correct for drift).
+compute_integrals=0;            % Compute the integral of the displacements in the time direction. Typically used if the reference frame is the previous frame (not recommended).
+subtract_median_position=0;     % If computing integrals or if the reference frame is not a relaxed frame, should subtract the time-median position considered as the relaxed one.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Parameters for the display of the experimental displacement: 
+%            Parameters for the display of the overlays:                  %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% scale factor for displacement vectors display:
-f_U=2;      % Same option used for the fitted field. 
-% Scale factor picture (if it becomes too big to fit on the screen, creates bugs):
-f_pic=3/4;    % Same option used for the fitted field. 
-% Should the overlay file be saved on harddrive:
-save_overlay_experimental=1;
-show_movie_experimental=0;
-single_layer=0;  % Same option used for the fitted field. 
-layer_saved=147; % Same option used for the fitted field. 
+f_U=2;                          % Scale factor for displacement vectors
+f_F=10;                         % Scale factor for force vectors
+f_pic=3/4;                      % Scale factor for pictures (if it becomes too big to fit on the screen, creates bugs)
+save_overlay_experimental=1;    % Should the experimental displacement overlays be saved on the harddrive
+show_movie_experimental=0;      % Should the experimental displacement overlays be shown in Matlab
+save_overlay_fitted=1;          % Should the reconstructed fields overlays be saved on the harddrive
+show_movie_fitted=0;            % Should the reconstructed fields overlays be shown in Matlab
+single_layer=0;                 % Set to 1 to save a single layer of the stack only
+layer_saved=0.5;                % Either the number of a layer (integer), or a value between 0 and 1 indicating a position ratio between the bottom and top layer.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Parameters for the displacement fitting by force-reconstruction: 
+%    Parameters for the displacement fitting by force-reconstruction:     %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Timepoints to process (default: 1:n_timepoints but can be reduced to test
-% parameters fast on just one or a few timepoints):
-timepoints_to_process=1;%:n_timepoints;%[1 15 30];
-% Weight parameters for the minimization problem:
-alpha_z=1;                  % Relative importance of good fitting on z values (value between 0->1 if z values should be ignored -> considered as reliable as xy values)
+% Timepoints to process (default: 1:n_timepoints-1 to reconstruct forces in
+% all frames except the reference one, but can be reduced to test parameters 
+% fast on just one or a few timepoints, or to analyze all frames if the median
+% displacement is used as a relaxed reference estimate):
+timepoints_to_process=1:n_timepoints-1;
+G=100;                      % Shear modulus, Pa.
+nu=0;                       % Poisson ratio
 alpha_F_cell_surface=1e-9;  % Relative importance of minimizing the forces on the cell surface 
 alpha_F_matrix=1e9;         % Relative importance of minimizing the forces compared to fitting the data in the matrix
-alpha_smoothing=5;
-% Mechanical parameters:
-G=70;      % Shear modulus, Pa.
-nu=0;       % Poisson ratio
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%       Parameters for the display of the reconstructed fields:           %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% scale factor for force vectors
-f_F=10;
-% Should the movie be shown in matlab
-show_movie_fitted=0;
-% Should the file be saved on harddrive (will be same layer as previously defined for the experimental field):
-save_overlay_fitted=1;
+alpha_smoothing=1;          % Relative importance of minimizing the difference between the force in a position of the cell surface and the average force on neighboring cell surface positions
+alpha_z=1;                  % Relative importance of good fitting on z values (value between 0->1 if z values should be ignored -> considered as reliable as xy values)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %      Parameters for the 4D display of the reconstructed fields          %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-plot_fraction=0.05;    % fractions of the vectors to be plotted. Start from low values or risk of crash. 
-patch_reduction=0.01; % fraction of the facets to be kept in the meshing of the cell surface (initial density is close to the pixel density). 
-f_U_3D=3;            % scale factor on the vector display. 
-gaussian_smoothing_stress_display=26;    % in um. Gaussian smoothing is applied to the field of tensors on the surface of the cell before using them to find the normal component. 
-type_of_color_display=1;    % 0: Norm of the pressure applied by the cell |stress*normal|. 1: Normal pressure normal*stress*normal 2: Principal stress (max |eigenvalue| of stress tensor). 
-normalize_color=1;    % If set to 1: normalize to the value below. If set to 0: normalize to max. 
-inverted_color_display=0;   % Default (0) is blue for low values, yellow for high values. Set to 1 to invert this. 
-minimum_color=-40;
-maximum_color=40;          % Value of the contraction used for the max color. % A good guess for this max color is mean(principal_stress(cell_surface),'omitnan')+std(")
+plot_fraction=0.05;                     % fraction of the vectors to be plotted. Start from low values or risk of crash. 
+patch_reduction=0.01;                   % fraction of the facets to be kept in the meshing of the cell surface (initial mesh density is close to the pixel density). 
+f_U_3D=3;                               % scale factor on the 3D displacement vector display. 
+gaussian_smoothing_stress_display=20;   % in um. Sigma for the Gaussian weighted average applied to the field of tensors on the surface of the cell to interpolate it on every cell surface mesh position. 
+type_of_color_display=1;                % 0: Norm of the stress/pressure applied by the cell defined as |stress_tensor*normal|. 1: Normal pressure defined as normal*stress_tensor*normal 2: Principal stress defined as the max eigenvalue of the stress tensor in norm. 
+normalize_color=1;                      % If set to 1: normalize to the value below. If set to 0: normalize to max. 
+inverted_color_display=0;               % Default (0) is blue for low values, red for high values. Set to 1 to invert this. 
+minimum_color=-40;                      % in Pa (same as pN/um^2). Value of the contraction used for the min color. % A good first guess for this min color is min(principal_stress(cell_surface),'omitnan')
+maximum_color=40;                       % in Pa (same as pN/um^2). Value of the contraction used for the max color. % A good first guess for this max color is max(principal_stress(cell_surface),'omitnan')
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%     The time and rotation is defined for any number of keyframes.       %
+%          The frames in between are a linear interpolation.              %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 fr=4;   % frame rate (not an input, just used below)
 tp=180;  % frames per rotation (not an input, just used below)
-nt=n_timepoints; % (not an input, just used below)
-% The time and rotation is defined for any number of keyframes. The frames in between take a linearly interpolated time and view.
-% % % Video from front view and no rotation nor interpolation:
+nt=n_timepoints-1; % (not an input, just used below)
+
+% % % Video from front view with no rotation nor interpolation:
 % key_frames=    [1  nt]; 
 % key_times=     [1  nt];
-% key_azimuths=  [40 54];
-% key_elevations=[70 50];
+% key_azimuths=  [50 50];
+% key_elevations=[60 60];
+
 % % Three repeats during full rotation:
 % key_frames=    [1  nt*fr  nt*fr+1  2*nt*fr 2*nt*fr+1 3*nt*fr]; 
 % key_times=     [1  nt     1       nt      1         nt     ];
 % key_azimuths=  [46 46+120 46+120+1 46+240  46+240+1  46+360 ];
 % key_elevations=[52 65     65       42      42        52     ];
-% % Just one timepoint:
+
+% Just one timepoint:
 key_frames=1;
 key_times=1;
 key_azimuths=46;
@@ -123,9 +112,8 @@ key_elevations=-52;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Parameters for the layer by layer display of the mesh dependent fields  %
+%       (Local pressure and normals are examples of such fields)          %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Display of the local pressure and normals are examples of such mesh
-% dependent fields
 f_P=2;
 P_fraction=0.2;
 
@@ -150,10 +138,11 @@ TFM_save_workspace
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %      For extraction of the average pressure in a user defined box       %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-pressure_definition=type_of_color_display;    % 0: Norm of the pressure applied by the cell |stress*normal|. 1: Normal pressure normal*stress*normal 2: Principal stress (max |eigenvalue| of stress tensor). 
-user_box=[30 40 10 25 15 30];   %xmin xmax ymin ymax zmin zmax
+pressure_definition=type_of_color_display;  % Same definitions as above are available.
+user_box=[30 40 10 25 15 30];               % xmin xmax ymin ymax zmin zmax. Typically, look at the 3D view, and use the zoom and/or axis() functions to find the coordinates of the box where data should be extracted. 
 TFM_6b_extract_pressure_data_in_box;
-P_box
+disp(P_box)
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %             To save the current figure at high resolution:              %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
